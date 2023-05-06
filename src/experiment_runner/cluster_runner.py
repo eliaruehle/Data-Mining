c5cfe118d8bd42853ca7a90416517e9f30ee7bf4
Elia Rühle <elia.ruehle@outlook.de>
2023-05-07 00:02:29 +0200

    loop in runner, multiple batch size clustering, fixes for performance, debugging

--- src/experiment_runner/cluster_runner.py
+++ src/experiment_runner/cluster_runner.py
@@ -1,7 +1,8 @@
 from datasets.loader import Loader
 from project_helper.method_types import CLUSTER_STRAT
+from project_helper.Logger import Logger
 from experiment_runner import BaseRunner
-from typing import Any, List
+from typing import Any, List, Tuple
 from clustering import (
     KMeansClustering,
     SpectralClustering,
@@ -9,7 +10,10 @@ from clustering import (
     OPTICSClustering,
     BaseClustering,
 )
-from side_handler import NoSuchClusterMethodError
+from side_handler import NoSuchClusterMethodError, ClusterFormatError
+from joblib import Parallel, delayed
+import numpy as np
+import pandas as pd
 
 
 class ClusterRunner(BaseRunner):
@@ -19,9 +23,14 @@ class ClusterRunner(BaseRunner):
 
     labels: List[str]
     cluster_obj: List[BaseClustering]
+    num_clusters: List[int]
 
     def __init__(
-        self, data: Loader, name: str, components: List[CLUSTER_STRAT]
+        self,
+        data: Loader,
+        name: str,
+        components: List[CLUSTER_STRAT],
+        num_clusters: List[int],
     ) -> None:
         """
         Initializes the ClusterRunner class.
@@ -39,7 +48,9 @@ class ClusterRunner(BaseRunner):
         --------
         None
         """
+        Logger.info(f"Create ClusterRunner named {name}!")
         super().__init__(data, name, components)
+        self.num_clusters = num_clusters
         self.labels = self.data.get_strategy_names()
         self.cluster_obj = self.create_cluster_objects()
 
@@ -61,10 +72,12 @@ class ClusterRunner(BaseRunner):
         for strat in self.components:
             match strat:
                 case 1:
-                    Strat: KMeansClustering = KMeansClustering("k_means", self.labels)
+                    Strat: KMeansClustering = KMeansClustering(
+                        "kmeans", self.labels, self.num_clusters
+                    )
                 case 2:
                     Strat: SpectralClustering = SpectralClustering(
-                        "spectral", self.labels
+                        "spec", self.labels, self.num_clusters
                     )
                 case 3:
                     Strat: DBSCANClustering = DBSCANClustering("dbscan", self.labels)
@@ -77,8 +90,48 @@ class ClusterRunner(BaseRunner):
             cluster_obj.append(Strat)
         return cluster_obj
 
-    def run(self):
-        pass
+    def run(self) -> None:
+        for dataset in self.data.get_dataset_names():
+            for metric in self.data.get_metric_names():
+                for hyper_tuple in self.data.get_hyperparameter_for_metric_filtering():
+                    data_vectors: List[np.ndarray] = list()
+                    for strategy in self.data.get_strategy_names():
+                        frame: pd.DataFrame = self.data.get_single_dataframe(
+                            strategy, dataset, metric
+                        )
+                        single_vec: pd.DataFrame = frame.loc[
+                            (frame["EXP_START_POINT"] == hyper_tuple[0])
+                            & (frame["EXP_BATCH_SIZE"] == hyper_tuple[1])
+                            & (frame["EXP_LEARNER_MODEL"] == hyper_tuple[2])
+                            & (frame["EXP_TRAIN_TEST_BUCKET_SIZE"] == hyper_tuple[3])
+                        ]
+                        single_vec = single_vec.iloc[:, :-9].dropna(axis=1)
+                        # proof if we collected some data, or data didn't exist
+                        if single_vec.empty:
+                            # continue loop
+                            Logger.info(
+                                f"Single vector was empty. Strategy {strategy} skipped."
+                            )
+                            pass
+                        else:
+                            data_vectors.extend(single_vec.to_numpy())
+                    if len(data_vectors) == self.data.NUM_STRATS:
+                        try:
+                            for clustering in self.cluster_obj:
+                                clustering.cluster(data_vectors)
+                            data_vectors.clear()
+                        except:
+                            Logger.info(
+                                ClusterFormatError("Error in clustering attempt!")
+                            )
+                            data_vectors.clear()
+                    else:
+                        data_vectors.clear()
+                break
+            break
+
+        for cluster_strat in self.cluster_obj:
+            cluster_strat.write_cluster_results()
 
     def get_components(self) -> List[str]:
         """
--- src/main.py
+++ src/main.py
@@ -9,68 +9,27 @@ from project_helper.Logger import Logger
 from pprint import pprint
 from project_helper.method_types import CLUSTER_STRAT
 from experiment_runner import ClusterRunner
-import os
+
 
 def main():
-    current_dir = os.getcwd()
-    project_root = os.path.abspath(os.path.join(current_dir, ".."))
-    os.chdir(project_root)
-    data = Loader("kp_test")
+    # initialize one data object
+    PROJECT_DATA: Loader = Loader("kp_test")
 
-    # kmeans_clustering = KMeansClustering("kmeans", data.get_strategy_names())
-    spectral_clustering = SpectralClustering(
-        "spectral_clustering", data.get_strategy_names()
+    # initialize experiment runners
+    CLUSTER_RUNNER = ClusterRunner(
+        PROJECT_DATA,
+        "clusterings",
+        [
+            CLUSTER_STRAT.KMEANS,
+            CLUSTER_STRAT.SPECTRAL,
+            CLUSTER_STRAT.OPTICS,
+            CLUSTER_STRAT.DBSCAN,
+        ],
+        [4],
     )
 
-    # fest: Dataset, Metric, -> search for Hyperparameters that are similar
-    tracker: int = 0
-    test: List[np.ndarray] = list()
-    for dataset in data.get_dataset_names():
-        for metric in data.get_metric_names():
-            if tracker % 10000 == 0:
-                # kmeans_clustering.write_cluster_results(4)
-                spectral_clustering.write_cluster_results(4)
-            for hyper_tuple in data.get_hyperparameter_for_metric_filtering():
-                for strategy in data.get_strategy_names():
-                    frame = data.get_single_dataframe(strategy, dataset, metric)
-                    to_append = frame.loc[
-                        (frame["EXP_RANDOM_SEED"] == hyper_tuple[0])
-                        & (frame["EXP_START_POINT"] == hyper_tuple[1])
-                        & (frame["EXP_BATCH_SIZE"] == hyper_tuple[2])
-                        & (frame["EXP_LEARNER_MODEL"] == hyper_tuple[3])
-                        & (frame["EXP_TRAIN_TEST_BUCKET_SIZE"] == hyper_tuple[4])
-                    ]
-                    to_append = to_append.iloc[:, :-9].dropna(axis=1)
-                    # print(to_append.to_numpy())
-                    # print(to_append.shape)
-                    # wenn to_append leer ist dann nicht hinzufügen
-                    # TODO: create an unpack function to unpack all arrays at one AL-cycle
-                    # TODO: if metric == selected_indices: skip the current loop -> not interesting for clustering
-                    if to_append.empty:
-                        pass
-                    else:
-                        test.extend(to_append.to_numpy())
-                # print(len(test))
-                if len(test) == len(data.get_strategy_names()):
-                    try:
-                        # kmeans_clustering.cluster(test, 4)
-                        spectral_clustering.cluster(test, 4)
-                    except:
-                        Logger.info(ClusterFormatError("multiple values per cycle"))
-                        test.clear()
-                        pass
-                    tracker += 1
-                    test.clear()
-                else:
-                    test.clear()
-            break
-        break
-
-    # kmeans_clustering.write_cluster_results(4)
-    # print(kmeans_clustering.get_similiarity_matrix().get_orderd_similarities())
-
-    # optics_clustering.write_cluster_results()
-    # print(optics_clustering.get_similiarity_matrix().get_orderd_similarities())
+    Logger.info(f"Start Running {str(CLUSTER_RUNNER)}")
+    CLUSTER_RUNNER.run()
 
 
 if __name__ == "__main__":
