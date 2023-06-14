aa41c32d0f5fa2e5770b895927212f4cb282b5dd
jembie <75253705+jembie@users.noreply.github.com>
2023-06-14 13:01:48 +0200

    Add ThreadPoolExecutor and rename function

--- src/seed_analysis/seed_analysis.py
+++ src/seed_analysis/seed_analysis.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import fnmatch
 import os
 import pprint
@@ -9,9 +10,7 @@ class Seed_Analysis:
     def __init__(self, file_path: str) -> None:
         self.file_path = file_path
         self.csv_files: dict[str, list[str]] = self.find_csv_files()
-        self.data_frames: dict[
-            str, list[pd.DataFrame]
-        ] = self.load_csv_into_data_frames()
+        self.data_frames: dict[str, list[pd.DataFrame]] = self.load_data_frames()
 
     def find_csv_files(self):
         csv_files = {}
@@ -33,20 +32,24 @@ class Seed_Analysis:
 
         return csv_files
 
+    def load_data_frames(self):
+        data_frames = {}
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for metric, files in self.csv_files.items():
+                future_to_df = {
+                    executor.submit(pd.read_csv, file, compression="xz"): file
+                    for file in files
+                }
+                data_frames[metric] = []
+                for future in concurrent.futures.as_completed(future_to_df):
+                    data_frames[metric].append(future.result())
+        return data_frames
+
     def pretty_print_csv_files(self):
         pprint.pprint(self.csv_files)
 
     def pretty_print_data_frames(self):
         pprint.pprint(self.data_frames)
-
-    def load_csv_into_data_frames(self):
-        data_frames = {}
-
-        for metric, files in self.csv_files.items():
-            data_frames[metric] = [
-                pd.read_csv(file, compression="xz") for file in files
-            ]
-        return data_frames
 
 
 seed = Seed_Analysis("kp_test")
