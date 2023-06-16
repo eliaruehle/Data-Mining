5fa0347ac8f6b7ad18a0252d100b15141abb9c1f
Elia Rühle <elia.ruehle@outlook.de>
2023-06-16 10:32:05 +0200

    new approach for paramters

---
+++ remake/helper/process_hpc_csv.py
@@ -0,0 +1,40 @@
+import csv
+from typing import Set, List
+import pandas as pd
+import os
+import multiprocessing as mp
+
+
+def process_file(path:str) -> Set:
+    if os.path.exists(path):
+        df = pd.read_csv(path)
+        tuples = set(df.apply(tuple, axis=1))
+        print("number entries: ", len(tuples), path)
+        if len(tuples) < 100:
+            return set()
+        return tuples
+    else:
+        raise ValueError("Path not exists!")
+
+def main() -> None:
+    root = "../results/"
+    if os.path.exists(root):
+        files: List[str] = sorted([os.path.join(root, file.name) for file in os.scandir(root) if not file.is_dir()])
+    else:
+        raise ValueError("Path not exists!")
+
+    with mp.Pool(mp.cpu_count()) as pool:
+        results = pool.map(process_file, files)
+    pool.close()
+    final = [sets for sets in results if sets]
+    final = set.intersection(*final)
+
+    with open(os.path.join(root, "all_hyperparameters.csv"), "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["EXP_START_POINT", "EXP_BATCH_SIZE", "EXP_LEARNER_MODEL", "EXP_TRAIN_TEST_BUCKET_SIZE"])
+        for tuple_item in final:
+            writer.writerow(tuple_item)
+
+
+if __name__ == "__main__":
+    main()
--- remake/helper/retrieve_unique_hyperparams.py
+++ remake/helper/retrieve_unique_hyperparams.py
@@ -2,12 +2,10 @@
 This script contains code to retrieve all the hyperparameters for which there are stable runs and which can be used for
 methods like a mini-batch clustering.
 """
-import sys
 from typing import List, Set, Tuple
 import os
 import pandas as pd
 import csv
-import time
 import sys
 import multiprocessing as mp
 
@@ -27,11 +25,14 @@ def scrape_all_files(root: str) -> List[str]:
     return files
 
 
-def get_hyperparameters_from_single_file(path: str) -> Set[Tuple[int, int, int, int]]:
+def get_hyperparameters_from_single_file(path: str) -> Tuple[str, Set[Tuple[int, int, int, int]]]:
     df = pd.merge(pd.read_csv(path), all_hyper, on="EXP_UNIQUE_ID")
     df = df[
         [
+            "EXP_DATASET",
+            "EXP_RANDOM_SEED",
             "EXP_START_POINT",
+            "EXP_NUM_QUERIES",
             "EXP_BATCH_SIZE",
             "EXP_LEARNER_MODEL",
             "EXP_TRAIN_TEST_BUCKET_SIZE",
@@ -40,39 +41,37 @@ def get_hyperparameters_from_single_file(path: str) -> Set[Tuple[int, int, int, 
     result = set(df.apply(tuple, axis=1))
     if len(result) < 300:
         to_discard.append(path)
-        return set()
-    return result
+        return path, set()
+    return path, result
 
 
 def main(index: int):
-    print("in main")
     root: str = "/scratch/ws/0/vime121c-db-project/Extrapolation"
     #root: str = "../../kp_test_int/strategies"
     strategy = sorted([entry.name for entry in os.scandir(root) if entry.is_dir()])[index]
+    print("Strategy name:", strategy)
     files = scrape_all_files(os.path.join(root, strategy))
-    start = time.time()
-    print("start loop")
     with mp.Pool(mp.cpu_count()) as pool:
         results = pool.map(get_hyperparameters_from_single_file, files)
     pool.close()
-    results_filtered = [sets for sets in results if sets]
-    print(f"Finished in {time.time()-start} sec.")
-    results_final = set.intersection(*results_filtered)
+    results_filtered = [entry[1] for entry in results]
+    print([entry[0] + "\n" for entry in results])
+    results_final = set.union(*results_filtered)
 
     with open(f"/home/h9/elru535b/scratch/elru535b-workspace/Data-Mining/remake/results/hyperparameter_final_{index}.csv", "w", newline="") as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow(["EXP_START_POINT", "EXP_BATCH_SIZE", "EXP_LEARNER_MODEL", "EXP_TRAIN_TEST_BUCKET_SIZE"])
+        writer.writerow([
+            "EXP_DATASET",
+            "EXP_RANDOM_SEED",
+            "EXP_START_POINT",
+            "EXP_NUM_QUERIES",
+            "EXP_BATCH_SIZE",
+            "EXP_LEARNER_MODEL",
+            "EXP_TRAIN_TEST_BUCKET_SIZE",
+        ])
         for tuple_item in results_final:
             writer.writerow(tuple_item)
 
-    try:
-        with open("/home/h9/elru535b/scratch/elru535b-workspace/Data-Mining/remake/results/to_discard.csv", "w", newline="") as discard:
-            writer = csv.writer(discard)
-            writer.writerow(["PATH"])
-            for discard_path in to_discard:
-                writer.writerow(discard_path)
-    except:
-        print("Error occurred")
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
