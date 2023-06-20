e5a7f96c42ee05f5dc5e976b7df0902f16632965
AnnaLopatkina <lopaat1009@gmail.com>
2023-06-20 11:30:49 +0200

    added similarity plot for all strategies

--- src/clustering/plots.py
+++ src/clustering/plots.py
@@ -8,11 +8,21 @@ from similarity_matrix import SimilarityMatrix
 
 
 def create_heatmap(clustering_method: str, title: str):
-  data_origin = pd.read_csv('./src/cl_res/'+ clustering_method +'.csv', index_col=0)
-  data = SimilarityMatrix.from_csv(filepath='./src/cl_res/'+ clustering_method +'.csv').normalize().as_2d_list()
+  filepath = "./remake/results/final_cluster_normalized.csv"
+  labels = ['ALIPY_CORESET_GREEDY','ALIPY_DENSITY_WEIGHTED','ALIPY_GRAPH_DENSITY','ALIPY_RANDOM','ALIPY_UNCERTAINTY_LC'
+            ,'ALIPY_UNCERTAINTY_MM','LIBACT_DWUS','LIBACT_QUIRE','LIBACT_UNCERTAINTY_ENT','LIBACT_UNCERTAINTY_LC','LIBACT_UNCERTAINTY_SM',
+            'OPTIMAL_GREEDY_10','OPTIMAL_GREEDY_20','PLAYGROUND_BANDIT','PLAYGROUND_GRAPH_DENSITY','PLAYGROUND_INFORMATIVE_DIVERSE',
+            'PLAYGROUND_KCENTER_GREEDY','PLAYGROUND_MARGIN','PLAYGROUND_MIXTURE','PLAYGROUND_UNIFORM','SKACTIVEML_COST_EMBEDDING',
+            'SKACTIVEML_DAL','SKACTIVEML_DWUS','SKACTIVEML_QBC','SKACTIVEML_QBC_VOTE_ENTROPY','SKACTIVEML_QUIRE','SKACTIVEML_US_ENTROPY',
+            'SKACTIVEML_US_LC','SKACTIVEML_US_MARGIN','SMALLTEXT_BREAKINGTIES','SMALLTEXT_CONTRASTIVEAL','SMALLTEXT_EMBEDDINGKMEANS',
+            'SMALLTEXT_GREEDYCORESET','SMALLTEXT_LEASTCONFIDENCE','SMALLTEXT_LIGHTWEIGHTCORESET','SMALLTEXT_PREDICTIONENTROPY','SMALLTEXT_RANDOM']
 
-  fig, ax = plt.subplots(figsize=(12,10))
-  sns.heatmap(data, ax=ax, annot=True, yticklabels=data_origin.index, xticklabels=data_origin.columns, cbar_kws={'label': 'Similarity'}, cmap="YlGnBu")
+  #data_origin = pd.read_csv('./src/cl_res/'+ clustering_method +'.csv', index_col=0)
+  #data = SimilarityMatrix.from_csv(filepath='./src/cl_res/'+ clustering_method +'.csv').normalize().as_2d_list()
+  data = SimilarityMatrix.from_csv(filepath=filepath).as_2d_list()
+
+  fig, ax = plt.subplots(figsize=(30,20))
+  sns.heatmap(data, ax=ax, annot=True, yticklabels=labels, xticklabels=labels, cbar_kws={'label': 'Similarity'}, cmap="YlGnBu")
   plt.yticks(rotation=0)
   plt.xticks(rotation=45, ha='right')
   plt.title(title)
@@ -21,16 +31,5 @@ def create_heatmap(clustering_method: str, title: str):
   plt.savefig('./src/clustering/generated/new_plots/'+ clustering_method + '.png', dpi=300, bbox_inches='tight')
 
 if __name__ == "__main__":
-  #create_heatmap("kmeans_Iris_3_centers", "KMeans Iris 3 centrer")
-  #create_heatmap("kmeans_4_centers", "KMeans 4 centrer")
-  #create_heatmap("kmeans_5_centers", "KMeans 5 centrer")
-  #create_heatmap("optics_Iris", "OPTICS Iris")
-  #create_heatmap("dbscan_Iris", "DBSCAN Iris")
-  #create_heatmap("gaussian_mixture_Iris", "Gaussian Mixture Iris")
-  #create_heatmap("spec_Iris_3_centers", "Spectral Iris 3 centrer")
-  #create_heatmap("spec_Iris_4_cnt", "Spectral Iris 4 centrer")
-  #create_heatmap("spec_5_cnt", "Spectral 5 centrer")
-  #create_heatmap("spec_wine_origin_5_centers", "Spectral wine origin 5 centers")
-  #create_heatmap("optics_wine_origin", "OPTICS wine origin")
-  create_heatmap("kmeans_wine_origin_5_centers", "KMeans wine origin 5 centers")
+  create_heatmap("all", "all")
    
